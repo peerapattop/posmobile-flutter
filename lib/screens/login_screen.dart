4da787669cf4080d3ccb65c41f764c180376f008
peerapattop <peerapatza401@gmail.com>
2024-09-01 21:37:44 +0700

    create login page and update input

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,15 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:posmobile/screens/signup_screen.dart';
 
-class Login extends StatefulWidget {
+import '../widgets/custom_button.dart';
+import '../widgets/custom_input.dart';
+
+class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
-}
-
-class _LoginState extends State<Login> {
-  @override
   Widget build(BuildContext context) {
-    return const Text('Login');
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          height: size.height,
+          child: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              Positioned(
+                top: 0,
+                left: 0,
+                child: Image.asset(
+                  "assets/images/main_top.png",
+                  width: size.width * 0.3,
+                ),
+              ),
+              Positioned(
+                bottom: 0,
+                right: 0,
+                child: Image.asset(
+                  "assets/images/login_bottom.png",
+                  width: size.width * 0.4,
+                ),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'LOGIN',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  SvgPicture.asset("assets/icons/login.svg",
+                      height: size.height * 0.35),
+                  const SizedBox(height: 30),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 40, left: 40, bottom: 25),
+                    child: CustomInput(
+                      prefixIcon: Icons.person,
+                      hintText: 'Email',
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 40, left: 40),
+                    child: CustomInput(
+                      obscureText: true,
+                      prefixIcon: Icons.lock,
+                      suffixIcon: Icons.visibility,
+                      hintText: 'Password',
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        right: 40, left: 40, top: 25, bottom: 10),
+                    child: CustomButton(
+                      text: 'LOGIN',
+                      onPressed: () {},
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      const Text(
+                        "Don't have Account ?",
+                        style: TextStyle(color: Colors.deepPurple),
+                      ),
+                      const SizedBox(width: 10),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const SignUp(),
+                            ),
+                          );
+                        },
+                        child: const Text(
+                          "Sign Up",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.deepPurpleAccent),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/widgets/custom_input.dart
+++ lib/widgets/custom_input.dart
@@ -1,10 +1,47 @@
 import 'package:flutter/material.dart';
 
 class CustomInput extends StatelessWidget {
-  const CustomInput({super.key});
+  final String hintText;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType keyboardType;
+  final IconData? prefixIcon;
+  final IconData? suffixIcon;
+
+  const CustomInput({
+    super.key,
+    this.hintText = '',
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType = TextInputType.text,
+    this.prefixIcon,
+    this.suffixIcon,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+        color: const Color(0xFFF7DCF9),
+        borderRadius: BorderRadius.circular(29),
+      ),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: InputBorder.none,
+          contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+          prefixIcon: prefixIcon != null
+              ? Icon(prefixIcon, color: Colors.deepPurple)
+              : null,
+          suffixIcon: suffixIcon != null
+              ? Icon(suffixIcon, color: Colors.deepPurple)
+              : null,
+        ),
+      ),
+    );
   }
 }
