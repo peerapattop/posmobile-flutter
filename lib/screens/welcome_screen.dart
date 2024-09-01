dda01b409196711d15874a7761b87743c37ffb42
peerapattop <peerapatza401@gmail.com>
2024-09-01 18:05:09 +0700

    add image in welcome screen

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,6 +5,17 @@ class Welcome extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    return SizedBox(
+      height: size.height,
+      width: double.infinity,
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            child: Image.asset("assets/images/main_top.png"),
+          )
+        ],
+      ),
+    );
   }
 }
