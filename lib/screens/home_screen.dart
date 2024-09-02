56bdde14a951f439ab484d65e31acfd218220f10
peerapattop <peerapatza401@gmail.com>
2024-09-02 22:23:27 +0700

    create bottom navigation and update data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:posmobile/screens/Profile/profile_screen.dart';
+import '../widgets/custom_bottom_navigation_bar.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,10 +10,31 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text('Home'),
+    Text('Cart'),
+    Text('Report'),
+    Profile()
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Home"),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
---
+++ lib/widgets/custom_bottom_navigation_bar.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+class CustomBottomNavigationBar extends StatelessWidget {
+  final int currentIndex;
+  final ValueChanged<int> onTap;
+
+  const CustomBottomNavigationBar({
+    required this.currentIndex,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      height: 80,
+      elevation: 0,
+      selectedIndex: currentIndex,
+      onDestinationSelected: onTap,
+      destinations: [
+        NavigationDestination(
+          icon: Icon(Iconsax.home, color: currentIndex == 0 ? Colors.purple : Colors.grey),
+          label: "Home",
+        ),
+        NavigationDestination(
+          icon: Icon(Iconsax.shopping_cart, color: currentIndex == 1 ? Colors.purple : Colors.grey),
+          label: "Cart",
+        ),
+        NavigationDestination(
+          icon: Icon(Iconsax.receipt, color: currentIndex == 2 ? Colors.purple : Colors.grey),
+          label: "Report",
+        ),
+        NavigationDestination(
+          icon: Icon(Iconsax.user, color: currentIndex == 3 ? Colors.purple : Colors.grey),
+          label: "Profile",
+        ),
+      ],
+    );
+  }
+}
