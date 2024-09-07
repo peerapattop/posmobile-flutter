4f8c32840395cdfa47daa944927b20b7b47eb487
peerapattop <peerapatza401@gmail.com>
2024-09-07 16:55:33 +0700

    update report page

--- lib/screens/Report/report_screen.dart
+++ lib/screens/Report/report_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import '../../widgets/custom_appbar.dart';
 
 class Report extends StatefulWidget {
@@ -10,16 +9,67 @@ class Report extends StatefulWidget {
 }
 
 class _ReportState extends State<Report> {
+  // กำหนดค่าตัวเลือกใน dropdown
+  List<String> items = ['วันนี้', 'สัปดาห์นี้', 'เดือนนี้'];
+
+  // กำหนดสถานะเริ่มต้น
+  String? selectedValue;
+
+  @override
+  void initState() {
+    super.initState();
+    // ตั้งค่าเริ่มต้นเป็นตัวเลือกแรกในรายการ
+    selectedValue = items[0];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppbar(
+      appBar: const CustomAppbar(
         mainAxisAlignment: MainAxisAlignment.center,
         title: "สรุปยอดขาย",
         backgroundColor: Colors.deepPurple,
         toolbarHeight: 70,
       ),
-      body: Text("data"),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Pie Chart'),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.deepPurple, width: 2.0),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  width: 130,
+                  child: DropdownButtonHideUnderline(
+                    child: DropdownButton<String>(
+                      value: selectedValue,
+                      isExpanded: true,
+                      items: items.map((String item) {
+                        return DropdownMenuItem<String>(
+                          value: item,
+                          child: Text(item),
+                        );
+                      }).toList(),
+                      onChanged: (String? newValue) {
+                        setState(() {
+                          selectedValue = newValue;
+                        });
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            const Divider(height: 200),
+          ],
+        ),
+      ),
     );
   }
 }
